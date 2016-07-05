f62785bfd5cd2fc8d2ec9bcbb53a22b6a93cf90c
john <jldjld503@gmail.com>
2016-07-05 13:28:17 -0700

    new appkey created and hidden properly

---
+++ app/src/main/java/com/epicodus/androidindependentproject/Constants.java
@@ -0,0 +1,8 @@
+package com.epicodus.androidindependentproject;
+
+/**
+ * Created by john on 7/5/16.
+ */
+public class Constants {
+    public static final String APP_KEY = BuildConfig.APP_KEY;
+}
