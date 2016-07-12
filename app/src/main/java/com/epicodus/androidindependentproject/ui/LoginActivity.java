684aa1e55651b64acf9ef4486efcc44e63306f93
john <Guest@epicodus-23.local>
2016-07-12 16:00:17 -0700

    user login working with dialogue messages for failed login

--- app/src/main/java/com/epicodus/androidindependentproject/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/androidindependentproject/ui/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.epicodus.androidindependentproject.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.epicodus.androidindependentproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,26 +28,63 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.loginButton) Button mLoginButton;
     @Bind(R.id.createAccountButton) Button mCreateAccountButton;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
         mLoginButton.setOnClickListener(this);
         mCreateAccountButton.setOnClickListener(this);
+        createAuthProgressDialog();
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
     public void onClick(View view) {
         if (view == mLoginButton) {
-            String email = mEmailLogin.getText().toString();
-            String password = mPasswordLogin.getText().toString();
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            intent.putExtra("email", email);
-            intent.putExtra("password", password);
-            startActivity(intent);
-            finish();
+            loginWithPassword();
+
         }
         if (view == mCreateAccountButton) {
             Intent intent = new Intent(LoginActivity.this, CreateProfileActivity.class);
@@ -46,5 +92,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             finish();
         }
     }
+
+    private void loginWithPassword() {
+        String email = mEmailLogin.getText().toString().trim();
+        String password = mPasswordLogin.getText().toString().trim();
+        if (email.equals("")) {
+            mEmailLogin.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordLogin.setError("Password cannot be blank");
+            return;
+        }
+        mAuthProgressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
 }
+
 
